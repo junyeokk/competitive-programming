ad929935677c72093697d4cba017059e3b4172c4
junyeokk <cjh4302@gmail.com>
2022-10-03 21:37:05 +0900

    feat: convention

---
+++ convention.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define INF 1000000000
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+
+// memset(memo, -1, size of memo);
+// memset(arr, 0, sizeof arr);
+
+void solve() {
+
+}
+
+int main() {
+    int t;
+    cin >> t;
+
+    while(t--) {
+        solve();
+    }
+}
