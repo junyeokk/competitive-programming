b665481d38f0c948362e42dacb2f269e969a0097
junyeokk <cjh4302@gmail.com>
2022-10-03 21:37:50 +0900

    feat: codechef MY1STCONTEST

---
+++ codechef/MY1STCONTEST.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve() {
+    int n, a, b;
+    cin >> n >> a >> b;
+    cout << n - a << " " << n - a - b << '\n';
+}
+
+int main() {
+    solve();
+}
