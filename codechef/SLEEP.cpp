4e1551829189dbef25ddc4dcbde5a785cc7bd38f
junyeokk <cjh4302@gmail.com>
2022-10-03 21:38:14 +0900

    feat: codechef SLEEP

---
+++ codechef/SLEEP.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve() {
+    int x;
+    cin >> x;
+    if(x < 7) cout << "YES\n";
+    else cout << "NO\n";
+}
+
+int main() {
+    int t;
+    cin >> t;
+
+    while(t--) {
+        solve();
+    }
+}
