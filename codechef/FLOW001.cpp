ae3453dfa8035b71018e71d5171a0ce0a6dbea22
junyeokk <cjh4302@gmail.com>
2022-10-03 21:37:32 +0900

    feat: codechef FLOW001

---
+++ codechef/FLOW001.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int t, n, m;
+    cin >> t;
+
+    for(int i = 0; i < t; i++) {
+        cin >> n >> m;
+        cout << n + m << '\n';
+    }
+}
