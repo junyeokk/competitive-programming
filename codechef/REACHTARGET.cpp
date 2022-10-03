60fecf45897f9560c08c35c8166ec8e594adf795
junyeokk <cjh4302@gmail.com>
2022-10-03 21:38:05 +0900

    feat: codechef REACHTARGET

---
+++ codechef/REACHTARGET.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve() {
+    int x, y;
+    cin >> x >> y;
+
+    cout << x - y << '\n';
+}
+
+int main() {
+    int t;
+    cin >> t;
+
+    while(t--) {
+        solve();
+    }
+}
