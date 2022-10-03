fe707b4e7992a5066282b577a6056125319ab05c
junyeokk <cjh4302@gmail.com>
2022-10-03 22:54:59 +0900

    feat: codechef SEMCOURSES

---
+++ codechef/SEMCOURSES.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve() {
+    int x, y, z;
+    cin >> x >> y >> z;
+    cout << x * y * z << '\n';
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+}
