3a791e006e3e7c23e26965c9eea6ea989abaa83c
junyeokk <cjh4302@gmail.com>
2024-03-19 00:03:21 +0900

    refactor: 삭제된 파일 반영

--- programmers/level2/mining_minerals.cpp
+++ programmers/level2/mining_minerals.cpp
@@ -1,3 +1,26 @@
-//
-// Created by Junhyeok CHAE on 3/18/24.
-//
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef unsigned long long ll;
+vector<ll> v;
+
+void solve() {
+    sort(v.begin(), v.end());
+    cout << v.front() << " " << v[(v.size() - 1) / 2] << " " << v.back();
+}
+
+void input() {
+    int n;
+    cin >> n;
+    v.push_back(n);
+}
+
+int main() {
+    int t;
+    cin >> t;
+
+    while(t--) {
+        input();
+    }
+    solve();
+}
--- test.cpp
+++
@@ -1,3 +0,0 @@
-//
-// Created by Junhyeok CHAE on 2023/03/07.
-//
