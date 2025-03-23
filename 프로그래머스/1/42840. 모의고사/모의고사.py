8faedcc2040b9663666cd1266ad59fa2a5895b0f
Junhyeok Chae <cjh4302@gmail.com>
2025-03-23 22:36:17 +0900

    [level 1] Title: 모의고사, Time: 8.81 ms, Memory: 9.36 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,13 +1,17 @@
 def solution(answers):
     res = []
     
+    # 수포자들 패턴
     zzikki = [
         [1, 2, 3, 4, 5],
         [2, 1, 2, 3, 2, 4, 2, 5],
         [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
     ]
+    
+    # 점수 저장할 리스트
     score = [0] * 3
     
+    # 각 수포자들의 패턴과 정답이 얼마나 일치하는지 확인
     for i in range(0, 3):
         idx = 0
         for answer in answers:
@@ -15,6 +19,7 @@ def solution(answers):
                 score[i] += 1
             idx += 1
 
+    # 가장 높은 점수를 가진 수포자들의 번호를 찾아서 리스트에 담음
     for i in range(0, 3):
         if max(score) == score[i]:
             res.append(i + 1)
