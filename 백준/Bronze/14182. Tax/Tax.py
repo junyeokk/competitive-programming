c5478ca132ce2b34a21e94e0efa06780f63471e0
Junhyeok Chae <cjh4302@gmail.com>
2026-02-28 19:49:52 +0900

    [Bronze III] Title: Tax, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/14182. Tax/Tax.py
@@ -0,0 +1,12 @@
+while(True):
+    n = int(input())
+    
+    if n == 0:
+        break
+    
+    if 1000000 <= n <= 5000000:
+        print(int(n * 0.9))
+    elif n >= 5000000:
+        print(int(n * 0.8))
+    else:
+        print(n)
