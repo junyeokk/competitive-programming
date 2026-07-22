69dbc2fc02a7aeb147abf2db3b3474a4c73a918a
Junhyeok Chae <cjh4302@gmail.com>
2026-07-22 20:12:35 +0900

    [level 2] Title: 타겟 넘버, Time: 127.73 ms, Memory: 11.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -0,0 +1,15 @@
+def solution(numbers, target):
+    answer = 0
+    
+    def dfs(idx, tot):
+        nonlocal answer
+        
+        if idx == len(numbers):
+            if tot == target:
+                answer += 1
+            return
+        dfs(idx + 1, tot + numbers[idx])
+        dfs(idx + 1, tot - numbers[idx])
+    
+    dfs(0, 0)
+    return answer
